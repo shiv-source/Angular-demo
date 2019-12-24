fdbcc672ab05f54a01c99d5755bb3eee58c35904
shiv-source <shivk7466@gmail.com>
2019-12-25 05:28:18 +0530

    Functionallity added

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./demo.component.css']
 })
 export class DemoComponent implements OnInit {
+    displayText=true;
+    displayName=false;
+    displayMsg=false;
+    colorss ='blue';
+    colors=["red","green","orange","blue","white"]
+
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+  onclick(){
+    if(this.displayText){
+      this.displayText=false;
+    }
+    else{
+      this.displayText=true;
+    }
+  }
+  onClickMouse(){
+    if(this.displayName){
+      this.displayName=false;
+    }
+    else{
+      this.displayName=true;
+    }
+  }
+  onClickMsg(){
+    if(this.displayMsg){
+      this.displayMsg=false;
+    }
+    else{
+      this.displayMsg=true;
+    }
   }
 
 }
